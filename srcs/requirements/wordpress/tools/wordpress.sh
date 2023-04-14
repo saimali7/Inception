89e39ba75a78a01d71c7516c0f98b6c5b5b61990
Saim Ali <saim@Saims-MacBook-Air.local>
2023-04-14 10:56:22 +0400

    added git ignore

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,2 +1,3 @@
 sudo -u www-data wp core install --url=localhost --title=newsite --admin_user=saim --admin_password=test123 --admin_email=saim@gmail.com --path='/var/www/wordpress'
+sudo -u www-data wp user create user2 user2@gmail.com --user_pass=user123 --role=author --path='/var/www/wordpress'
 php-fpm7.3 -F --nodaemonize
